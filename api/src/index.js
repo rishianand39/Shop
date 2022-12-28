9baabb2e805086dcbbdd34c8e09a424397736554
rishianand39 <rishi.rn818@gmail.com>
2022-12-28 20:42:54 +0530

    payment route added

--- api/src/index.js
+++ api/src/index.js
@@ -12,7 +12,7 @@ import auth from "./controllers/user.controller.js";
 import product from "./controllers/product.controller.js";
 import cart from "./controllers/cart.controller.js";
 import order from "./controllers/order.controller.js";
-
+import payment from "./controllers/payment.controller.js";
 // home route
 app.get("/",(req,res)=>{
     return res.status(200).json("Welcome to shop api")
@@ -21,15 +21,17 @@ app.get("/",(req,res)=>{
 // products 
 app.use("/api/product", product)
 
-
 // authentication 
 app.use("/api/auth", auth)
-
 
 // cart 
 app.use("/api/cart", cart)
 
 // order
 app.use("/api/order", order)
+
+// payment
+app.use("/api/pay", payment)
+
 
 export default app;
