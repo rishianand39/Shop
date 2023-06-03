8dca9380b02484ebc88794785deaa7dda4d1e315
rishianand39 <rishi.rn818@gmail.com>
2023-06-03 11:20:31 +0530

    formatted

--- api/src/server.js
+++ api/src/server.js
@@ -1,15 +1,14 @@
-import app from "./index.js"
-import dotenv from "dotenv"
+import app from "./index.js";
+import dotenv from "dotenv";
 import connect from "./configs/db.js";
 dotenv.config();
 
-
-const PORT =process.env.PORT || 8000;
-app.listen(PORT,async()=>{
-    try {
-         connect();
-        console.log(`server running on port ${PORT}`);
-    } catch (error) {
-        console.log(error);
-    }
-})
+const PORT = process.env.PORT || 8000;
+app.listen(PORT, async () => {
+  try {
+    connect();
+    console.log(`server running on port ${PORT}`);
+  } catch (error) {
+    console.log(error);
+  }
+});
